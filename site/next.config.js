b9d9da9347cbc3028741377331722cec44c9b9e0
djarbz <30350993+djarbz@users.noreply.github.com>
2024-02-12 08:53:54 -0600

    Update next.config.js
    Customize site parameters for my instance.

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'DJArbz Technologies',
+    description: 'The unofficial store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://djarbz.github.io/kasm-registry/',
+    contactUrl: 'https://github.com/djarbz/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
